7f32692af4b95ef3f0bed5b830942df86885cece
sergey.podzemelnykh <sergey.podzemelnykh@onlinesup.com>
2021-10-21 14:57:00 +0300

    Done *

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var regex = regexp.MustCompile(`[a-zA-Zа-яА-Я]+-?[a-zA-Zа-яА-Я]*`)
+
+func Top(top int, text string) (res []string) {
+	matches := regex.FindAllString(text, -1)
+
+	if matches == nil {
+		return nil
+	}
+
+	wordFrequencies := map[string]int{}
+	for _, word := range matches {
+		wordFrequencies[strings.ToLower(word)]++
+	}
+
+	frequencyWords := map[int][]string{}
+	for word, frequency := range wordFrequencies {
+		frequencyWords[frequency] = append(frequencyWords[frequency], word)
+	}
+
+	frequencies := []int{}
+	for frequency := range frequencyWords {
+		frequencies = append(frequencies, frequency)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(frequencies)))
+	for _, frequency := range frequencies {
+		sort.Strings(frequencyWords[frequency])
+		res = append(res, frequencyWords[frequency]...)
+	}
+
+	if len(wordFrequencies) < top {
+		top = len(wordFrequencies)
+	}
+	return res[:top]
 }
