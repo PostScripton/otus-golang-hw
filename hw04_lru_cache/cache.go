9716dae38d798218d179193c09fea57b092873a4
sergey.podzemelnykh <sergey.podzemelnykh@onlinesup.com>
2021-11-01 19:00:50 +0300

    Done LRU_Cache !*

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -27,4 +25,40 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	listItem, ok := c.items[key]
+	cached := cacheItem{key: key, value: value}
+
+	if ok {
+		listItem.Value = cached
+		c.queue.MoveToFront(listItem)
+	} else {
+		if c.queue.Len() == c.capacity {
+			old := c.queue.Back()
+			c.queue.Remove(old)
+			delete(c.items, old.Value.(cacheItem).key)
+		}
+		listItem = c.queue.PushFront(cached)
+	}
+	c.items[key] = listItem
+
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	listItem, ok := c.items[key]
+
+	if ok {
+		c.queue.MoveToFront(listItem)
+		return listItem.Value.(cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = map[Key]*ListItem{}
 }
